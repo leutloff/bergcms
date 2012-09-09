40a75d7b3efc7a439086738e38317164d35f035a
Christian Leutloff <leutloff@sundancer.oche.de>
2012-09-09 23:24:58 +0200

    Script to update and build the submodules where needed.

---
+++ update_and_build_submodules.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+# Updates the submodules and builds them where needed.
+#
+# Copyright 2012 Christian Leutloff <leutloff@sundancer.oche.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+SOURCEDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CTEMPLATEDIR=$SOURCEDIR/src/external/ctemplate
+
+echo "Updating the submodules in $SOURCEDIR..."
+
+pushd $SOURCEDIR
+git submodule init
+git submodule update
+popd
+
+echo "Build the submodule in $CTEMPLATEDIR..."
+
+pushd $CTEMPLATEDIR
+
+./configure --prefix $CTEMPLATEDIR
+make clean
+make
+make install
+
+popd
+
+exit 0;
