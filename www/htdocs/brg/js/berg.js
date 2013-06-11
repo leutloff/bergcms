10d3edc4bce50e2f605e58c7e0e732d390abbe1c
Christian Leutloff <leutloff@sundancer.oche.de>
2013-06-11 22:33:58 +0200

    Added more content to short reference.

--- www/htdocs/brg/js/berg.js
+++ www/htdocs/brg/js/berg.js
@@ -23,16 +23,36 @@ var brg = {
         shortref += "    <a href=\"javascript:;\" id=\"refpara\">Absätze<\/a>,";
         shortref += "    <a href=\"javascript:;\" id=\"refregions\">Bereiche<\/a>    ";
         shortref += "    <\/p>";
-        shortref += "    <p id=\"contentwords\">\\textbf{fett}";
+        shortref += "    <p id=\"contentwords\">";
+        shortref += "    \\textbf{<strong>fett<\/strong>}<br \/>";
+        shortref += "    \\mbox{nicht umbrechen}<br \/>";
+        shortref += "    &gt;BILD#Titel#c#Datei#b:160mm#Fotograf<br \/>";
+        shortref += "    \\url{http:\/\/feg.de}, \\href{mailto:redaktion@aachen.feg.de}{redaktion@aachen.feg.de}, oder \\href{mailto:termine@aachen.feg.de}{\\protect\\nolinkurl{termine@aachen.feg.de} }<br \/>";
+        shortref += "    &gt;i#Autor<br \/>";
         shortref += "    <\/p>";
-        shortref += "    <p id=\"contentpara\">\\newline";
+        shortref += "    <p id=\"contentpara\">";
+        shortref += "    &gt;sg#4 Standardschriftgröße 0..9<br \/>";
+        shortref += "    &gt;zd#0 oder &gt;zd#1.0 Standardzeilenabstand<br \/>";
+        shortref += "    \\newline<br \/>";
+        shortref += "    <br \/>";
+        shortref += "    dehnbaren Abstand einfügen, z.B. zwischen Monaten in der Terminliste oder bei Adressänderungen:<br \/>";
+        shortref += "    \\vspace{2ex plus 1ex minus 1ex}";
         shortref += "    <\/p>";
         shortref += "    <p id=\"contentregions\">";
-        shortref += "       &gt;spalten#2#zweispaltig<br \/>";
+        shortref += "    &gt;1#Bereichsüberschrift, wie Einladungen oder Rückblick<br \/>";
+        shortref += "    &gt;2#Artikelüberschrift<br \/>";
+        shortref += "    &gt;3#Zwischenüberschriften in einem Artikel (fett und Abstand)<br \/>";
+        shortref += "    <br \/>";
+        shortref += "    &gt;!# Neue Spalte beginnen<br \/>";
+        shortref += "    &gt;+# Neue Seite beginnen<br \/>";
+        shortref += "    &gt;SPALTEN#1# ab hier einspaltig<br \/>";
+        shortref += "    &gt;SPALTEN#2# ab hier zweispaltig<br \/>";
         shortref += "    <\/p>";
+        shortref += "";
 
         $("#brg-short-reference").replaceWith(shortref);
         
+        // show/hide parts of the short reference
         $("#contentwords").hide();
         $("#contentpara").hide();
         $("#contentregions").hide();
