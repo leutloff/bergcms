929472d5f36fbf325b4a25b339181f6931690ef2
Christian Leutloff <leutloff@sundancer.oche.de>
2013-07-15 23:07:14 +0200

    Add remark that the return code from the perl script can not be changed from 0.

--- src/test/input/simpleperlscript.pl
+++ src/test/input/simpleperlscript.pl
@@ -7,4 +7,5 @@ my $cwd = getcwd();
 print $cwd."\n";
 
 # return 0 - that is the expected return code from this simple script
+# hmm - really another value is wished, but does not work 8-(
 exit(0);
