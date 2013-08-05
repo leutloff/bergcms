c41f12dfd9374343bd3bfcc110ab565d2e19b58e
Christian Leutloff <leutloff@sundancer.oche.de>
2013-08-05 13:15:05 +0200

    make_zip.sh: delete the CMake cache file CMakeCache.txt from last build to ensure a fresh configuration.

--- make_zip.sh
+++ make_zip.sh
@@ -4,9 +4,13 @@ HOSTNAME=$(hostname)
 BUILDDIR=$SOURCEDIR/build-mk-$HOSTNAME
 echo "Build project and make package (in $BUILDDIR)..."
 
-
 mkdir -p $BUILDDIR
 pushd $BUILDDIR
+
+# remove cache from last build to ensure a fresh configuration
+if [ -r CMakeCache.txt ]; then
+    rm -f CMakeCache.txt
+fi
 
 cmake -DCMAKE_VERBOSE_MAKEFILE=FALSE -DCMAKE_BUILD_TYPE=Distribution -DBoost_DEBUG=FALSE ../src
 make package berg_extract
