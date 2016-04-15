c47fa44095aad6ad3dc2b15c5c02c54560fb88ad
Christian Leutloff <leutloff@sundancer.oche.de>
2016-04-15 22:49:29 +0200

    Reenable tests with the existing databases. Relative paths for images are already used (closes #15).

--- www/cgi-bin/brg/t/run_tests.sh
+++ www/cgi-bin/brg/t/run_tests.sh
@@ -1,31 +1,33 @@
 #!/bin/bash -e
 SOURCEDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo "Running some tests for the perl scripts (started in $SOURCEDIR)..."
+echo "Running some tests for the perl scripts (starting in $SOURCEDIR)..."
 
 
 pushd $SOURCEDIR
 
-echo && echo "***   run funtion tests ...   ***"
-#perl -I$SOURCEDIR/.. pex.pl
+echo && echo && echo "***   run funtion tests ...   ***"
 mkdir -p BERG
 ln -fs $SOURCEDIR/../pex.pl BERG/PEX.pm
 perl test_pex.pl
 perl test_pex_columns.pl
 
-echo && echo "***   run tests on input database...   ***"
+echo && echo && echo "***   run tests on input database single_article.csv   ...   ***"
 INPUTFILE=$SOURCEDIR/../../../../src/test/input/single_article.csv
 OUTPUTFILE=$SOURCEDIR/output/single_article.tex
 EXPECTEDFILE=$SOURCEDIR/expected/single_article.tex
 
-#$SOURCEDIR/../pex.pl $INPUTFILE $SOUTPUTFILE $OUTPUTFILE
-#cat $OUTPUTFILE | grep -v '^\% Program' | diff $EXPECTEDFILE -
+$SOURCEDIR/../pex.pl $INPUTFILE $SOUTPUTFILE $OUTPUTFILE
+echo && echo "Compare output with expected $EXPECTEDFILE ..."
+cat $OUTPUTFILE | grep -v '^\% Program' | grep -v '^\% DB' | diff $EXPECTEDFILE -
 
+echo && echo && echo "***   run tests on input database some_articles.csv   ...   ***"
 INPUTFILE=$SOURCEDIR/../../../../src/test/input/some_articles.csv
 OUTPUTFILE=$SOURCEDIR/output/some_articles.tex
 EXPECTEDFILE=$SOURCEDIR/expected/some_articles.tex
 
 $SOURCEDIR/../pex.pl $INPUTFILE $SOUTPUTFILE $OUTPUTFILE
-#TODO reenable check: cat $OUTPUTFILE | grep -v '^\% Program' | grep -v '^\% DB' | diff $EXPECTEDFILE -
+echo && echo "Compare output with expected $EXPECTEDFILE ..."
+cat $OUTPUTFILE | grep -v '^\% Program' | grep -v '^\% DB' | diff $EXPECTEDFILE -
 
 popd
 
