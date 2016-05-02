b0ea51d71015d00dbd1768a61d0a49dbc93f4a3a
Christian Leutloff <leutloff@sundancer.oche.de>
2016-05-02 23:37:15 +0200

    Fix expected begin{document}.

---
+++ src/lib/ProcessStep.cpp
@@ -0,0 +1,6 @@
+#include "ProcessStep.h"
+
+ProcessStep::ProcessStep()
+{
+
+}
---
+++ src/lib/ProcessStep.h
@@ -0,0 +1,11 @@
+#ifndef PROCESSSTEP_H
+#define PROCESSSTEP_H
+
+
+class ProcessStep
+{
+public:
+    ProcessStep();
+};
+
+#endif // PROCESSSTEP_H
