bcd029df322f2fb0509a97c92384b122bbdfe4d8
Christian Leutloff <leutloff@sundancer.oche.de>
2016-05-02 23:38:53 +0200

    Start thinking about a class for the process steps.

--- src/lib/ProcessStep.cpp
+++ src/lib/ProcessStep.cpp
@@ -1,5 +1,7 @@
 #include "ProcessStep.h"
 
+using namespace berg;
+
 ProcessStep::ProcessStep()
 {
 
--- src/lib/ProcessStep.h
+++ src/lib/ProcessStep.h
@@ -1,11 +1,56 @@
-#ifndef PROCESSSTEP_H
-#define PROCESSSTEP_H
+/**
+ * @file ProcessStep.h
+ * Contains a single step of a process with commands and results.
+ *
+ * Copyright 2016 Christian Leutloff <leutloff@sundancer.oche.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#if !defined(BERGCMS_PROCESSSTEP_H)
+#define BERGCMS_PROCESSSTEP_H
 
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#   pragma once
+#endif
+
+#include <string>
+
+namespace berg
+{
 
 class ProcessStep
 {
+    /// Shell script command similar to executed command.
+    std::string command;
+    /// The absolute path to the executable.
+    std::string programPath;
+
+    std::string result;
+    std::string resultCode;
+    std::string resultLog;
+
 public:
     ProcessStep();
+
+    /// Checks that the executable is available and has execute bits set.
+    /// Additionally the used paths and input files may be checked.
+    bool CheckPrerequisites();
+    /// Perform the desired operation.
+    bool Execute();
+
 };
 
-#endif // PROCESSSTEP_H
+}
+
+#endif // BERGCMS_PROCESSSTEP_H
