bfd820ac0b2f445626b9cfcbc7eacd237762e18d
Christian Leutloff <leutloff@sundancer.oche.de>
2016-05-20 11:46:27 +0200

    Create the doc directory to store the generated PDF documentation when missing. Ensure failing script in case of errors.

--- build_documentation.sh
+++ build_documentation.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
-echo "Update .cls file"
+echo "***   Update .cls file   ***"
 
 pushd latex/class_berg
 ./update_cls_pdf.sh
 popd
+mkdir -p www/htdocs/brg/doc
 cp latex/class_berg/generated/berg.cls www/cgi-bin/brg/br/
 cp latex/class_berg/generated/berg.pdf www/htdocs/brg/doc/
 
-echo "Generate User Documentation..."
+echo "***   Generate User Documentation   ***"
 
 pushd doc/redakteur
 pdflatex redakteurin.tex
